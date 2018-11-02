44540730d85ffbfe700233e3abb27491d5008529
mohan3d <mohan3d94@gmail.com>
2018-11-02 21:30:26 +0200

    feat: provide implementation for DefaultParse

--- slideshare/parser.go
+++ slideshare/parser.go
@@ -2,6 +2,8 @@ package slideshare
 
 import (
 	"io"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 // Parser is the interface that wraps the basic Images method.
@@ -11,3 +13,31 @@ import (
 type Parser interface {
 	Images(r io.Reader) ([]string, error)
 }
+
+const (
+	slideSelector    = "img.slide_image"
+	imageURLSelector = "data-full"
+)
+
+type defaultParser struct {
+}
+
+func (p *defaultParser) Images(r io.Reader) ([]string, error) {
+	d, err := goquery.NewDocumentFromReader(r)
+
+	if err != nil {
+		return nil, err
+	}
+	var urls []string
+
+	d.Find(slideSelector).Each(func(i int, s *goquery.Selection) {
+		if url, exists := s.Attr(imageURLSelector); exists {
+			urls = append(urls, url)
+		}
+	})
+	return urls, nil
+}
+
+// DefaultParser used when no implementation
+// is provided for Parser interface.
+var DefaultParser = &defaultParser{}
